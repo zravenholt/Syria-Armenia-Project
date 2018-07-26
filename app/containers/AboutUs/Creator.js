8e96c3b6a277e716e3481e33f5395f411d2d6cee
Zane Ravenholt <zane@zane.local>
2018-07-26 07:53:09 -0700

    overflow about us creators

--- app/containers/AboutUs/Creator.js
+++ app/containers/AboutUs/Creator.js
@@ -4,10 +4,11 @@ const Creator = styled.div`
   position: relative;
   width: 30%;
   margin: 10px;
-  height: 1250px;
+  height: 1000px;
   z-index: 3;
   background-color: white;
   border-radius: 5px;
+  overflow: scroll;
 `;
 
 export default Creator;
